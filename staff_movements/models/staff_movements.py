3eeaa47a24821fffa96258188fa69e6f8e987b08
labodoo-dev <labodoodev@odoo.com>
2026-07-09 11:15:29 +0200

    [IMP] intermediate version of list and form view

--- staff_movements/models/staff_movements.py
+++ staff_movements/models/staff_movements.py
@@ -7,4 +7,12 @@ class StaffMovements(models.Model):
                     This module is intended to replace the shitty google sheets in next arrivals
                     """
 
-    user = fields.Char()
+    legal_name = fields.Char()
+    mode = fields.Selection(
+        selection=[
+            ("entry", "Entry"),
+            ("departures", "Departures"),
+            ("change_of_position", "Change of position")
+        ]
+    )
+    effective_date = fields.Date()
